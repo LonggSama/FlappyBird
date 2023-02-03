d31fcd484f5ac48458631d24e5c0b49d8efd8be4
Long Se7en <longnvh@se7engamestudio.com>
2023-02-03 16:41:46 +0700

    Player Control & Background move

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,23 +4,63 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    private Rigidbody2D birdRb;
+
+    public float flyForce = 10f;
+    //public float gravity = -9.8f;
+    public bool isDie;
+
+    //private Vector3 direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        birdRb = GameObject.FindGameObjectWithTag("Bird").GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
+        ActiveAnimator();
+        if (!isDie)
         {
-            this.gameObject.GetComponent<Animator>().enabled = false;
+            BirdFly();
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Obstacle")) 
+        {
+            isDie = true;
+        }
+    }
+
+    void ActiveAnimator()
+    {
+        if (isDie)
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    public void BirdFly()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            birdRb.AddForce(Vector3.up * flyForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                birdRb.AddForce(Vector3.up * flyForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject[] bird;
-    [SerializeField] float flyForce;
-
+    
     // Start is called before the first frame update
 
     private void Awake()
     {
         Invoke("CreateBird", 0f);
     }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        BirdFly();
-    }
 
     void CreateBird()
     {
         int birdIndex = Random.Range(0, bird.Length);
         Instantiate(bird[birdIndex], bird[birdIndex].transform.position, bird[birdIndex].transform.rotation, this.transform);
-    }
-
-    void BirdFly()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            transform.Translate(Vector3.up * flyForce * Time.deltaTime);
-        }
     }
 }
